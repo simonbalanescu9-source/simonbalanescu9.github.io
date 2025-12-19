b02f32fea07d909f6ebb0301d1bdb529e76c284d
simonbalanescu9-source <simonbalanescu9@gmail.com>
2025-12-19 09:13:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -696,12 +696,3 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
-animate();
-
-// Resize
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
